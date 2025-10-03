a13e2335c7fe34c62ecc4fa783d06f989fc84e1e
WUQ-man <wuqmanyt@gmail.com>
2025-10-03 15:37:53 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,25 @@
+from flask import Flask, request
+from telegram import Update
+from telegram.ext import Application
+
 import os
-from flask import Flask
-from threading import Thread
-from WUQpublisher_Bot import main  # همون فایلی که کد رباتت توشه
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("RENDER_EXTERNAL_URL") + "/webhook"
 
 app = Flask(__name__)
+application = Application.builder().token(BOT_TOKEN).build()
 
-@app.route('/')
-def home():
-    return "Bot is alive ✅"
-
-def run_flask():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    application.update_queue.put(update)
+    return "ok"
 
 if __name__ == "__main__":
-    # اجرای Flask تو یه Thread جدا
-    Thread(target=run_flask).start()
-    # اجرای ربات
-    main()
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        webhook_url=WEBHOOK_URL
+    )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
